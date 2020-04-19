2a22f248eb0c3667cbcad6cfe8c7cd33f0f0557f
1432junaid <1432junaid@gmail.com>
2020-04-19 20:33:39 +0530

    new

---
+++ coch-task/task016x2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int min(int x,int y){return (x < y) ?x : y;}
+int min_jump(vector<int>&arr){
+	int *jump = new jumps[]
+
+}
+int main(){
+	vector<int>arr = {2,3,1,1,4};
+	
+	min(arr)
+}
+
+
