cdf95092497805cdd1e6a2c5dd3dcf3e80c60da7
1432junaid <1432junaid@gmail.com>
2020-04-24 11:39:15 +0530

    both 19x1 and 19x2

--- coch-task/task019x1.c
+++ coch-task/task019x1.c
@@ -4,34 +4,34 @@ void printer(int *,int);
 void permu(int *,int);
 void swap(int*,int*);
 int sum_arr(int *,int);
+
 int main(){
 	int a[] = {1,2,3};
 	int len = sizeof(a)/sizeof(a[0]);
-	printer(a,len);
+//	printer(a,len);
 	int result;
 //	printf("\n");
-//	permu(a,len);
+	permu(a,len);
 //	printf("%d\n",sum_arr(a,len) );
 //	result = pow(10,3);
-	result = sum_arr(a,len);
-	printf("%d\n",result);
+//	result = sum_arr(a,len);
+//	printf("%d\n",result);
 	return 0;
 
-}/*
+}
+
 void permu(int a[],int len){
-	int arr[len];
-	for(int i =0; i<len; i++){
-		int arr[i] = a[i];
-	}
-	for(int i =0; i < len ; i++ ){
-		int key = arr[i];
-		arr[0] = key;
-		for(int j = len-1; j >= 1; j--){
-			swap(&a[j],&a[j-1]);
-			printer(a,len);
+	for(int i; i<len ;i++){
+	//	scanf("%d",&a[i]);
+		for(int j = 1;j <= len; j++){
+			for(i = 0; i< len-1; i++){
+				swap(&a[i], &a[i+1]);
+				printer(a,len);
+			}
 		}
 	}
-}*/
+}
+
 void printer(int a[],int len){
 	for(int i= 0; i<len; i++){
 		printf("%d\t",a[i]);
@@ -45,13 +45,15 @@ void swap(int *a,int *b){
 		*a 	= *b;
 		*b	= temp;
 }
-
+/*
 int sum_arr(int a[],int len){
-	int sum = 0 ;
-	for(int i =0; i<len; i++,len--){
-		int res = pow(10,len);
-		sum += a[i] * res;
+	int sum1 = 0 ;
+	int m = len;
+	for(int i =0; i<len; i++,m--){
+		int res = pow(10 , m);
+		sum1 += a[i] * res;
 	}
-	return sum;
+	return sum1;
 }
 
+*/
